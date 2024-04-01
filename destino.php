463a987b126c339639ade108242d9c5d868b8a46
Lucio Gonzalez <lucgosa2@gmail.com>
2024-04-01 17:33:01 +0200

    first commit

---
+++ destino.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+	echo "ADIOS MUNDO!!!";
+	echo "<p><img src='planetas/marte.jpg' width='30%' /></p>";
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+	echo "<p>HOLA MUNDO!!!</p>";
+	echo "<p><img src='planetas/tierra.jpg' width='30%' /></p>";
+	echo "<a href='destino.php'>avanzar</a>";
+?>
+</body>
+</html>
